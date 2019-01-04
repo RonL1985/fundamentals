03ea483a350e890b9014af28025b5fcb2f4a3fc0
Ronald Luna <ronaldluna1@gmail.com>
2019-01-04 14:19:48 -0800

    moving old stuff to github

---
+++ variables-and-data.js
@@ -0,0 +1,14 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+let add = 1 + 2;
+let sub = 2-1;
+let mult = 2*4;
+let divisoin = 4 / 2;
+let mod = 5 % 2;
+
+let string = "sting 1 " + "sting 2";
+
+console. log(sting);
+
+
